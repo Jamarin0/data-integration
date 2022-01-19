d049cb5edb815cfe927567f4615cb2e18fbfd606
Jamarin0 <jgjsguilherme@gmail.com>
2022-01-18 21:59:40 -0300

    add: create server two

--- server1.js
+++ server1.js
@@ -1,8 +1,23 @@
-import { createServer} from 'http'
+import { createServer } from "http";
+import { parse } from "url";
+import {randomUUID} from 'crypto'
 
-const PORT = 3000
-async function handler( request, response){
-    return response.end('hey')
+const PORT = 3000;
+async function handler(request, response) {
+  if (request.method === "GET" && request.url.includes("products")) {
+    const {
+      query: { productName },
+    } = parse(request.url, true);
+    const result = {
+        id:randomUUID(),
+        productName
+    }
+
+    return response.end(JSON.stringify(result));
+  }
+  return response.end("hey");
 }
 
-createServer(handler).listen(PORT, () => console.log(`Products API is running at ${PORT}`))
+createServer(handler).listen(PORT, () =>
+  console.log(`Products API is running at ${PORT}`)
+);
